118258e502080d4bc9e8dd312438b3a2703b4088
Kaj Siebert <kaj@k-si.com>
2021-12-24 14:35:49 +0100

    Initial

---
+++ lw_common/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LwCommonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lw_common'
---
+++ lw_site/views.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+
+
+@login_required
+def index(request):
+    return redirect('/updates')
+
