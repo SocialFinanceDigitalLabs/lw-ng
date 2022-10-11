110e5af36e283716db976192810fe82ac2f81cea
Francisco Macedo <franciscovcbm@gmail.com>
2022-10-11 16:01:39 +0100

    cleanup: remove all unused files

--- lw_yp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LwYpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lw_yp'
--- lw_yp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path(f'{p["id"]}/', p["func"], name=p["id"]) for p in views.page.all.values()
-]
