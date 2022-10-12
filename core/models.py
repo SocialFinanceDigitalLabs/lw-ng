ebb88db83f694173fdf54074c4e293e992a88cb3
Matthew Pugh <pughmds@cyram.org>
2022-10-12 15:35:09 +0100

    Formatting fixes

--- core/models.py
+++ core/models.py
@@ -2,72 +2,118 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
+
 class ChecklistQuestionAnswer(models.IntegerChoices):
     NO = 0, _("No")
     YES = 1, _("Yes")
     NOT_RELEVANT = 2, _("This isn't relevant to me")
 
+
 class NoteType(models.IntegerChoices):
     PA = 0, _("Pathway Plan PA")
     MANAGER = 1, _("Pathway Plan Manager")
 
+
 class PersonalAdvisor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    manager = models.ForeignKey(User, related_name="personal_advisors", null=True, on_delete=models.SET_NULL)
+    manager = models.ForeignKey(
+        User, related_name="personal_advisors", null=True, on_delete=models.SET_NULL
+    )
     is_manager = models.BooleanField()
+
 
 class YoungPerson(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    personal_advisors = models.ManyToManyField(PersonalAdvisor, related_name="young_persons")
+    personal_advisors = models.ManyToManyField(
+        PersonalAdvisor, related_name="young_persons"
+    )
+
 
 class Goal(models.Model):
-    young_person = models.ForeignKey(YoungPerson, on_delete=models.CASCADE, related_name="goals")
+    young_person = models.ForeignKey(
+        YoungPerson, on_delete=models.CASCADE, related_name="goals"
+    )
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    last_updated_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="updated_goals")
+    last_updated_by = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="updated_goals"
+    )
     completed = models.DateTimeField(null=True, blank=True)
     archived = models.DateTimeField(null=True, blank=True)
-    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="created_goals")
+    created_by = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="created_goals"
+    )
     title = models.TextField(max_length=50)
     description = models.TextField(max_length=500, null=True, blank=True)
+
 
 class Action(models.Model):
     goal = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name="actions")
     created = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="created_actions")
+    created_by = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="created_actions"
+    )
     completed = models.DateTimeField(null=True, blank=True)
     deadline = models.DateTimeField(null=True, blank=True)
     last_updated = models.DateTimeField(auto_now=True)
-    last_updated_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="updated_actions")
+    last_updated_by = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="updated_actions"
+    )
     description = models.TextField(max_length=500)
-    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="owned_actions")
+    owner = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="owned_actions"
+    )
+
 
 class Checklist(models.Model):
     title = models.TextField(max_length=500)
 
+
 class Question(models.Model):
     text = models.TextField(max_length=500)
-    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE, related_name="checklist_questions")
+    checklist = models.ForeignKey(
+        Checklist, on_delete=models.CASCADE, related_name="checklist_questions"
+    )
+
 
 class ChecklistResponse(models.Model):
-    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE, related_name="checklist_instances")
+    checklist = models.ForeignKey(
+        Checklist, on_delete=models.CASCADE, related_name="checklist_instances"
+    )
     created = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="created_checklist_responses")
-    young_person = models.ForeignKey(YoungPerson, on_delete=models.CASCADE, related_name="checklist_responses")
-    note = models.TextField(max_length=500, null=True, blank=True)
-
-class ChecklistQuestionResponse(models.Model):
-    checklist_response = models.ForeignKey(ChecklistResponse, on_delete=models.CASCADE, related_name="checklist_question_response_instance")
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="question_responses")
-    answer = models.PositiveSmallIntegerField(
-        choices=ChecklistQuestionAnswer.choices
+    created_by = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="created_checklist_responses",
+    )
+    young_person = models.ForeignKey(
+        YoungPerson, on_delete=models.CASCADE, related_name="checklist_responses"
     )
     note = models.TextField(max_length=500, null=True, blank=True)
 
+
+class ChecklistQuestionResponse(models.Model):
+    checklist_response = models.ForeignKey(
+        ChecklistResponse,
+        on_delete=models.CASCADE,
+        related_name="checklist_question_response_instance",
+    )
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="question_responses"
+    )
+    answer = models.PositiveSmallIntegerField(choices=ChecklistQuestionAnswer.choices)
+    note = models.TextField(max_length=500, null=True, blank=True)
+
+
 class Note(models.Model):
-    young_person = models.ForeignKey(YoungPerson, on_delete=models.CASCADE, related_name="notes")
+    young_person = models.ForeignKey(
+        YoungPerson, on_delete=models.CASCADE, related_name="notes"
+    )
     submitted = models.DateTimeField(auto_now_add=True)
-    submitted_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="submitted_notes")
+    submitted_by = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="submitted_notes"
+    )
     note_type = models.PositiveSmallIntegerField(
         choices=NoteType.choices, null=True, blank=True
     )
